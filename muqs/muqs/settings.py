d1b10173175a5d2a5a4310c65b4aece35c5d22c7
miluciuke <44410179+miluciuke@users.noreply.github.com>
2020-09-25 09:47:59 +0100

    Update settings.py

--- muqs/muqs/settings.py
+++ muqs/muqs/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = STATIC_DIR
 
 
 
